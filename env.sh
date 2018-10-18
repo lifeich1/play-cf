9784a60ee7067ef8ab9ef5a36af5472bd2b80dfc
lifeich1 <lifeich0@gmail.com>
2018-10-18 10:34:00 +0800

    :sparkles: shell environment with helpful functions
    New feature for easier make & run (issue #2)

---
+++ env.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+export CFHOME=`pwd -P`
+export CFCODE=""
+NEW=$CFHOME/new.sh
+PRINT="$CFHOME/print.sh"
+
+cfworkdir() {
+    echo "$CFHOME/src/$CFCODE"
+}
+
+into() {
+    if [ -d `cfworkdir` ]; then
+        cd `cfworkdir`
+    else
+        echo "problem does not set or initialized."
+    fi
+}
+
+uptop() {
+    cd $CFHOME
+}
+
+atproblem() {
+    export CFCODE=$1
+}
+
+newproblem() {
+    tmp=$OLDPWD
+    olddir=`pwd`
+    uptop
+    $NEW $1 $2 && atproblem $1
+    cd $olddir
+    export OLDPWD=$tmp
+}
+
+
+editall() {
+    dir=`cfworkdir`
+    vim $dir/_io.cc $dir/type.h $dir/*.cpp
+}
+
+editmain() {
+    vim `cfworkdir`/*.cpp
+}
+
+editio() {
+    vim `cfworkdir`/_io.cc
+}
+
+edittype() {
+    vim `cfworkdir`/type.h
+}
+
+runtest() {
+    if [ -d `cfworkdir` ]; then
+        test -z $TMPDIR && TMPDIR='.'
+        echo '/*=== compiling ===*/'
+        g++ -I `cfworkdir` `cfworkdir`/*.cc `cfworkdir`/*.cpp -Wall \
+            -o $TMPDIR/a.out
+        echo '/*=== running ===*/'
+        $TMPDIR/a.out
+    else
+        echo "problem does not set or initialized."
+    fi
+}
+
+pcode() {
+    tmp=$OLDPWD
+    olddir=`pwd`
+    uptop
+    $PRINT $CFCODE
+    cd $olddir
+    export OLDPWD=$tmp
+}
+
+cpcode() {
+    pcode | pbcopy && echo 'copied.'
+}
